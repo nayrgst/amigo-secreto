9404cac0ee037bec083958a2849994ce7b4fcecd
Ryan <ryan567.pr@gmail.com>
2025-02-12 10:37:26 -0300

    :recycle: refactor: Refatorada a tela de login usando zod e o Form do shadcn

--- actions/login.ts
+++ actions/login.ts
@@ -1,33 +1,33 @@
-"use server";
+// "use server";
 
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 
-export type LoginState = {
-  success: boolean | null;
-  message?: string;
-};
+// export type LoginState = {
+//   success: boolean | null;
+//   message?: string;
+// };
 
-export async function login(previousState: LoginState, formData: FormData) {
-  const supabase = await createClient();
+// export async function login(previousState: LoginState, formData: FormData) {
+//   const supabase = await createClient();
 
-  const email = formData.get("email") as string;
+//   const email = formData.get("email") as string;
 
-  const { error } = await supabase.auth.signInWithOtp({
-    email,
-    options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/confirm `,
-    },
-  });
+//   const { error } = await supabase.auth.signInWithOtp({
+//     email,
+//     options: {
+//       emailRedirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/confirm `,
+//     },
+//   });
 
-  if (error) {
-    return {
-      success: false,
-      message: error.message,
-    };
-  }
+//   if (error) {
+//     return {
+//       success: false,
+//       message: error.message,
+//     };
+//   }
 
-  return {
-    success: true,
-    message: "Verifique seu e-mail para confirmar o login.",
-  };
-}
+//   return {
+//     success: true,
+//     message: "Verifique seu e-mail para confirmar o login.",
+//   };
+// }
